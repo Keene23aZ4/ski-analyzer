bf506d37ea25b70587e23745935c46996619b781
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-10-24 01:49:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def set_background():
         """
         <style>
         .stApp {
-            background-image: url("static/1704273575813.jpg");
+            background-image: url("/static/1704273575813.jpg");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
@@ -98,6 +98,7 @@ if uploaded_file:
             mime="video/mp4"
 
         )
+
 
 
 
