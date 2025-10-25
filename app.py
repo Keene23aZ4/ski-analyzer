6f5049d295b04b95fc2d56e6ca516181bbce94ce
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-10-26 00:17:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ translations = {
     "日本語": {
         "title": "滑走動作分析システム",
         "sidebar_title": "",
-        "caption": "骨格構造と関節角度の変化を可視化",
+        "caption": "できること：骨格構造、関節角度、前傾角度、内傾角度の変化を可視化",
         "background": "動画の背景の有無を選択",
         "background_options": ["表示", "非表示"],
         "select_all": "すべての関節角度を選択する",
@@ -150,4 +150,5 @@ if uploaded_file:
             file_name="analyzed_ski_video.mp4",
             mime="video/mp4"
         )
+
 
