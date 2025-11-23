812ca430fe4f859f345d145444c79038336b34c4
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-11-23 21:23:30 +0900

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -166,7 +166,7 @@ def process_video(input_path, progress_callback=None, show_background=True, sele
                     
                     if np.isnan(inclination_s):
                         turn_phase = "--"
-                    elif inclination_s <= 10.0:
+                    elif inclination_s <= 11.0:
                         turn_phase = "Neutral"
                     else:
                         left_knee_sum = left_knee_abduction + left_knee_s
@@ -277,6 +277,7 @@ def process_video(input_path, progress_callback=None, show_background=True, sele
 
     final_output = merge_audio(input_path, temp_output_path)
     return final_output
+
 
 
 
