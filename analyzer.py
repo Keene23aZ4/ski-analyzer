e052b62724d3cf2d4b4c61e4e0e51565a945b725
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-12-08 16:21:44 +0900

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -346,29 +346,29 @@ def process_video(input_path, progress_callback=None, show_background=True, sele
                     ["Inclination Angle", "--"]
                 ] 
                 img_pil = Image.fromarray(canvas)
-                    draw = ImageDraw.Draw(img_pil)
-                    font_path="static/BestTen-CRT.otf"
-                    font = ImageFont.truetype(font_path, 20)
+                draw = ImageDraw.Draw(img_pil)
+                font_path="static/BestTen-CRT.otf"
+                font = ImageFont.truetype(font_path, 20)
+            
+                cell_height = 40
+                start_x = 30
+                start_y = canvas.shape[0] - len(grid_data)*cell_height - 30
+
                 
-                    cell_height = 40
-                    start_x = 30
-                    start_y = canvas.shape[0] - len(grid_data)*cell_height - 30
+              
+                for i, (label, value) in enumerate(grid_data):
+                    y_pos = start_y + i * cell_height
 
-                    
-                  
-                    for i, (label, value) in enumerate(grid_data):
-                        y_pos = start_y + i * cell_height
-
-                        top_left = (start_x, start_y + i * 40)
-                        bottom_right = (start_x + 300, start_y + (i + 1) * 40)     
-                        draw.text((35, y_pos+10), label, font=font, fill=(255,255,255))
-                        draw.text((200, y_pos+10), value, font=font, fill=(255,255,255))
-                        cv2.rectangle(canvas, top_left, bottom_right, (0, 0, 0), -1)
-                        cv2.rectangle(canvas, top_left, bottom_right, (255, 255, 255), 1)
-                    canvas = np.array(img_pil)
-                    
-                    box_width, box_height = 300, 100
-                            
+                    top_left = (start_x, start_y + i * 40)
+                    bottom_right = (start_x + 300, start_y + (i + 1) * 40)     
+                    draw.text((35, y_pos+10), label, font=font, fill=(255,255,255))
+                    draw.text((200, y_pos+10), value, font=font, fill=(255,255,255))
+                    cv2.rectangle(canvas, top_left, bottom_right, (0, 0, 0), -1)
+                    cv2.rectangle(canvas, top_left, bottom_right, (255, 255, 255), 1)
+                canvas = np.array(img_pil)
+                
+                box_width, box_height = 300, 100
+                        
 
                                                            
            # 書き出し
