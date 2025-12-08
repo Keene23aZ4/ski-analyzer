867af76c6b2da3a1e51da0952b31cb44f96f0966
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-12-08 16:58:00 +0900

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -345,6 +345,8 @@ def process_video(input_path, progress_callback=None, show_background=True, sele
                     ["Torso Tilt", "--"],
                     ["Inclination Angle", "--"]
                 ] 
+                y_pos = start_y + i * cell_height
+                top_left = (start_x, start_y + i * 40)
                 cv2.rectangle(canvas, top_left, bottom_right, (0, 0, 0), -1)
                 cv2.rectangle(canvas, top_left, bottom_right, (255, 255, 255), 1)
                 img_pil = Image.fromarray(canvas)
