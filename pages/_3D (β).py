8c55859b7deea0b7d2bd00081651316c3a0d3edf
Keene23aZ4 <kentoyamaza17@gmail.com>
2025-12-09 22:04:02 +0900

    Update _3D (β).py

--- pages/_3D (β).py
+++ pages/_3D (β).py
@@ -158,7 +158,7 @@ if uploaded is not None:
 # Three.js 部分は通常文字列に分ける
     three_js_code = """
     <div id="container" style="width:100%; height:600px;"></div>
-    <script src="https://unpkg.com/three@0.152.2/build/three.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/three@0.152.2/examples/js/controls/OrbitControls.js"></script>
     <script>
       const container = document.getElementById('container');
