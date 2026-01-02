6e3f59ccfd5bea965e7c66ed75aa1c04051fc78d
Keene23aZ4 <kentoyamaza17@gmail.com>
2026-01-03 00:20:28 +0900

    Update _3D (β).py

--- pages/_3D (β).py
+++ pages/_3D (β).py
@@ -269,8 +269,8 @@ if uploaded:
             m.position.copy(pA);
             m.lookAt(pB);
         
-            const scaleX = baseRadius * 0.65;
-            const scaleY = baseRadius * 0.5;
+            const scaleX = baseRadius * 0.8;
+            const scaleY = baseRadius * 0.65;
         
             m.scale.set(scaleX / 0.06, scaleY / 0.06, length);
         
